13c4cb714bca5ce868dd17187ea1f6aea8202115
Carlos Diaz-Padron <cdpadron@twilio.com>
2013-05-22 09:59:59 -0700

    Sentence passes tests

--- app/models/sentence.rb
+++ app/models/sentence.rb
@@ -1,3 +1,9 @@
 class Sentence < ActiveRecord::Base
   attr_accessible :author_id, :content, :length, :story_id
+
+  belongs_to :author
+  belongs_to :story
+
+  validates_presence_of :content
+  validates_numericality_of :length, greater_than: 0
 end
---
+++ spec/models/sentence_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe Sentence do
+  it { should belong_to(:author) }
+  it { should belong_to(:story) }
+
+  it { should validate_presence_of(:content) }
+  it { should allow_value(5).for(:length) }
+  it { should allow_value(5.5).for(:length) }
+  it { should_not allow_value(0).for(:length) }
+  it { should_not allow_value(-1).for(:length) }
+  it { should_not allow_value(nil).for(:length) }
+end
