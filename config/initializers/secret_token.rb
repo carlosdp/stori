ccfd806781f2ff19db4c90936e7afcaa6aa2ad5f
Carlos Diaz-Padron <cdpadron@twilio.com>
2013-05-22 09:05:09 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stori::Application.config.secret_token = '33601e0510af54605dd1a82177df0cdf5159f3c205ca738e38ca7f4d73faf728f4e938e4cebc3c7f6d4fd8925f32787f25a28da6822f662b83d0d3c863b77358'
